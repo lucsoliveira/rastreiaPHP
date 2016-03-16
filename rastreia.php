761f27f92c96132c25240e303e625eb2b5070ce7
olucao <olucas1998@gmail.com>
2016-03-16 16:58:29 -0300

    1.1
    Melhora na análise do código de rastreio

--- rastreia.php
+++ rastreia.php
@@ -3,38 +3,35 @@
     //no metodo POST, pelo INPUT "cod"
     $codRastreio = $_POST['cod'];
 
-    //verifica se o campo do código de rastreio está vazio
-    if(empty($_POST['cod'])) {
+    //COLOCA COMO DEFINIÇÃO UTF-8 PARA CARACTERES ESPECIAIS
+    ini_set('default_charset', 'UTF-8');
 
-        echo '<div class="alert alert-danger" role="alert"><strong>Opsss!</strong> Adicione o código de rastreio.</div>';
+    //O SCRIPT TEM COMO BASE O GERADOR DE XML DO SITE AGENCIAIDEIAS, QUE É DISPONIBILIZADO GRATUITAMENTE
+    $xml = simplexml_load_file('http://developers.agenciaideias.com.br/correios/rastreamento/xml/'.$codRastreio.'');
 
-        //se o campo nao estiver vazio, sera verificado o código e gerado a tabela de dados
-    }else{
+    //faz uma verificação simples se o código de rastreio fornecido foi encontrado nos correios
+    //se retornar vazio, ira aperecer uma mensagem de erro
+    if($xml == ''){
 
-        //COLOCA COMO DEFINIÇÃO UTF-8 PARA CARACTERES ESPECIAIS
-        ini_set('default_charset', 'UTF-8');
+        echo '<div class="alert alert-danger" role="alert"><strong>Opsss!</strong> Código de rastreio com erro ou ainda não foi cadastrado no sistema dos correios.</div>';
 
-        //O SCRIPT TEM COMO BASE O GERADOR DE XML DO SITE AGENCIAIDEIAS, QUE É DISPONIBILIZADO GRATUITAMENTE
-        $xml = simplexml_load_file('http://developers.agenciaideias.com.br/correios/rastreamento/xml/'.$codRastreio.'');
+    }else{ 
+
         //o script ira criar uma tabela com dados do rastreio
         echo '<center><h3>Rastreio: '.$codRastreio.'</h3></center>';
         echo '<div class="row"><div class="col-md"><table class="table">';
-
         //divisão da tabela
-        echo "<thead><tr>
-        <th>Data</th><th>Local</th>
-        <th>Ação</th><th>Detalhes</th>
-        </tr></thead><tbody>";
+        echo "<thead> <tr> <th>Data</th> <th>Local</th> <th>Ação</th> <th>Detalhes</th> </tr> </thead> <tbody>";
 
         //Aqui o codigo faz um loop de dados se houver mais de uma informação recebida pelo XML dos correios
         foreach ($xml->evento as $rastreio) {
-        echo '<tr>';
-        echo '<td>'; echo $rastreio->data.'</td>'; echo '<td>'; echo $rastreio->local.'</td>'; 
-        echo '<td>'; echo $rastreio->acao.'</td>'; echo '<td>'; echo $rastreio->detalhes.'</td>';
-        echo '</tr>';
+            echo '<tr>';
+            echo '<td>'; echo $rastreio->data.'</td>'; echo '<td>'; echo $rastreio->local.'</td>'; 
+            echo '<td>'; echo $rastreio->acao.'</td>'; echo '<td>'; echo $rastreio->detalhes.'</td>';
+            echo '</tr>';
         }
-        
+
         //aqui é fechado o loop de dados recebidos
-        echo "</tbody></table>";
-        }
+        echo "</tbody></table>";        
+    } 
 ?>
